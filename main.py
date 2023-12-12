2b6a149ded1b6a09902a63895dfba1c62819929c
Ghora <public@mail.ru>
2023-12-12 22:12:02 +0700

    Change?

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from public.users import users_router
 
 app = FastAPI()
-
-@app.get('/')
-async def f_index():
-    return {"ФИО": "Емельяненков Григорий Алексеевич"}
-
-@app.get('/tools')
-async def f_indexT():
-    return "Бездарь!"
-
-@app.get('/users')
-async def f_indexT():
-    return {"Телефон": "+79628166881", "Электронная почта": "ghorickckrolik@mail.ru"}
+app.include_router(users_router)
+@app.get('/', response_class=PlainTextResponse)
+def f_index():
+    return "<b> Hello, User! </b>"
